240133ae06403d409f4e019c20670e8c5592ef00
Muyideen Odenike <modenike@gmail.com>
2016-09-06 15:27:23 -0500

    create ation and show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to 'show'
+    redirect_to articles_show(@article.id)
 
   end
 
