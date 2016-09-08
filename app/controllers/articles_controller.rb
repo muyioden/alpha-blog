18fa21d0c5926ab9f92839cbc68ac2e9cb5cf2fb
Muyideen Odenike <modenike@gmail.com>
2016-09-07 23:56:20 -0500

    flash messages modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     
     if @article.save
-      flash[:notice] = "You article saved successfully"
+      flash[:success] = "You article saved successfully"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
 
     if @article.update(article_params)
-      flash[:notice] = "Your article was successfully updated"
+      flash[:success] = "Your article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Your article was successfully deleted"
+    flash[:danger] = "Your article was successfully deleted"
     redirect_to articles_path
   end
 
