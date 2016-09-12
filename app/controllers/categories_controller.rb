9af3fb4c596212455beefca2eff7a0ace1abce88
Muyideen Odenike <modenike@gmail.com>
2016-09-12 14:49:29 -0500

    Added more feature for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,7 +23,8 @@ before_action :require_admin, except:[:index, :show]
   end
 
   def show
-
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 4)
   end
 
   private
